3d7238d3e29631e253bf08fe132d04dbb31c496d
YakovBoiev <iakovboiev@gmail.com>
2021-06-30 22:41:49 +0200

    finish coding task_1 chess

--- less5/script.js
+++ less5/script.js
@@ -1,37 +1,46 @@
 'use strict'
-const chess = document.getElementById('chess');
-let spam = ""
-for (let i = 0; i < 10; i++){
-    for (let j = 0; j < 10; j++){
-        spam += '<div class="cell"></div>';
+function boardChess() {
+    const chess = document.getElementById('chess');
+    let spam = ""
+    for (let i = 0; i < 10; i++) {        //  создаю поле 10 х 10
+        for (let j = 0; j < 10; j++) {
+            spam += '<div class="cell"></div>';
+        }
+        spam += '<br/>';
     }
-    spam += '<br/>';
-};
-chess.innerHTML = spam;
-const cell = Array.from(document.getElementsByClassName('cell'));
-for (let i = 0; i < cell.length; i++) {
-    cell[i].style.height = '50px';
-    cell[i].style.width = '50px';
-    cell[i].style.display = 'inline-block';
-    cell[i].style.border = "1px solid white"
-    cell[i].innerHTML = `${Math.floor(i / 10)}`
+    chess.innerHTML = spam;
+    const cell = Array.from(document.getElementsByClassName('cell')); //   задаю стиль всем клеткам
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].style.height = '50px';
+        cell[i].style.width = '50px';
+        cell[i].style.display = 'inline-block';
+        cell[i].style.border = "1px solid white"
+        cell[i].style.verticalAlign = "bottom"
+        cell[i].style.textAlign = "center"
     }
-
-for (let i = 10; i < cell.length - 10; i++) {
-    if (i % 10 != 0 && i % 10 != 9) {
-        cell[i].style.border = "1px solid black"
-        if (Math.floor(i / 10) % 2 === 0) {
-            if (i % 2 !== 0) {
+    for (let i = 10; i < cell.length - 10; i++) { // раскрашиваю поле без первой и последней строки
+        if (i % 10 !== 0 && i % 10 !== 9) {           // без первого и последнего столбца
+            cell[i].style.border = "1px solid black"
+            if (Math.floor(i / 10) % 2 === 0) {  // четные строки
+                if (i % 2 !== 0) {
+                    cell[i].style.background = 'black';
+                }
+            } else if (i % 2 === 0) {             // не четные строки
                 cell[i].style.background = 'black';
             }
-        } else if (i % 2 === 0) {
-            cell[i].style.background = 'black';
+        } else {                                          // в первом и последнем столбце нумерация строк
+            cell[i].innerHTML = Math.floor(i / 10);
         }
     }
+    let abc = ["", 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', ""] // обозначение столбцов
+    for (let i = 0; i < 10; i++) {
+        cell[i].innerHTML = abc[i]
+    }
+    for (let i = 90; i < 100; i++) {
+        cell[i].innerHTML = abc[i - 90]
+    }
 }
-
-
-
+boardChess()
 
 const basket = {
     products: [
@@ -57,6 +66,7 @@ const basket = {
 const elemBasket = document.getElementById('basket')
 let output = "Корзина пуста"
 if (basket.products.length > 0){
+    
     output = `В корзине ${basket.products.length} товаров на сумму ${basket.sumProductBasket()} рублей`
 }
 elemBasket.innerHTML = output;
