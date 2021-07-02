9afb92a581cf9d5b1805b4460755a9855f6fccbb
Yakov Boiev <user.name>
2021-07-02 17:17:00 +0300

    coding less6

--- less6/script.js
+++ less6/script.js
@@ -1,1 +1,23 @@
 'use strict'
+const cartProd = {
+    visual(prod) {
+        return `<div class="prod">
+                    <div><b>Наименование</b>: ${prod.name} + ${prod.model}</div>
+                    <div><b>Цена за шт.</b>: ${prod.price}</div>
+                 </div>`;
+}
+}
+
+const catalog =  {
+    products: [
+        {catalogName: "Computer", name: "Asus", model: "M509", price: 500},
+        {catalogName: "Tablet", name: "Samsung", model: "S6", price: 300},
+        {catalogName: "Phone", name: "Apple", model: "iPhone 12", price: 1000},
+        {catalogName: "Phone", name: "Apple", model: "iPhone 12", price: 1000}
+
+    ],
+
+    init() {
+
+    }
+}
